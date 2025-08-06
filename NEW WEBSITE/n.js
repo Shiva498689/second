27640d60f871d331d02fe645c4a98edd2e8a57ad
Shiva498689 <123shivadubey@gmail.com>
2025-08-06 20:57:33 +0530

    Removed folder 'NEW WEBSITE'

--- NEW WEBSITE/n.js
+++
@@ -1,9 +0,0 @@
-
-  function openTask(taskId) {
-    document.getElementById(taskId).classList.remove('hidden');
-  }
-
-  function closeTask(taskId) {
-    document.getElementById(taskId).classList.add('hidden');
-  }
-
