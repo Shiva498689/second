a589c10c5074035bae99bf4aa31fa41ccbe3fb31
Shiva498689 <123shivadubey@gmail.com>
2025-08-10 11:27:42 +0530

    guidelines

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-Guidelines to use the calcul
+Guidelines for giving input 
+    format : num1_+_num2
+    NOTE : here _ denotes space
+ ** if you want natural log then sample input should be like given below
+      input : ln("your number")
+ **for trigonometric functions enter angle in degrees for sin ,cos, tan 
+      && for inverse trigo output will be in radian 
+      
+ *** for calculating exponent of a number
+     Sample input: exp(____) your number in blank space 
--- learning phython/calculator scientific.py
+++ learning phython/calculator scientific.py
@@ -1,16 +1,7 @@
 import time
 import math
 
-print('''Guidelines for giving input 
-    format : num1_+_num2
-    NOTE : here _ denotes space
- ** if you want natural log then sample input should be like given below
-      input : ln("your number")
- **for trigonometric functions enter angle in degrees for sin ,cos, tan 
-      && for inverse trigo output will be in radian 
-      
- *** for calculating exponent of a number
-     Sample input: exp(____) your number in blank space ''')
+
 time.sleep(1)
 s= input("input  :")
 if(s[0:3]=="exp"):
