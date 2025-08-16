84cb73d785168a7bc864c2cb73cff5ea3f90c418
Shiva498689 <123shivadubey@gmail.com>
2025-08-16 15:32:53 +0530

    completed PS01

--- techsocprojects/PS01.py
+++ techsocprojects/PS01.py
@@ -22,6 +22,29 @@ def encode(s,val):
              str+=c
     print("Output :",str)
 
+def decode(s,val):
+    length= len(s)
+    str=""
+    for i in range(length):
+        c=s[i]
+        if(ord(c)>=65 and ord(c)<=90 ):
+            ascii=ord(c)
+            if(ascii - val >= 65):
+                str+=chr(ascii-val)
+            else:
+                 n=91-(65-(ascii-val)) 
+                 str+=chr(n)
+        elif(ord(c)>=97 and ord(c)<=122):
+            
+                ascii=ord(c)
+                if(ascii - val >= 97):
+                     str+=chr(ascii-val)
+                else:
+                     n=122-(97-(ascii-val)) 
+                     str+=chr(n)
+        else:
+             str+=c
+    print("Output :",str)
 
 inp = input('''if you want to encode a message please enter "encode" 
      else if you want to "decode" than enter decode :''')
@@ -29,3 +52,7 @@ if(inp=="encode"):
      os=input("Please enter your message :")
      num= int(input("Please enter the number from which you want to shift :"))
      encode(os,num)
+if(inp=="decode"):
+     os=input("Please enter your message :")
+     num= int(input("Please enter the number from which you want to shift or decode  :"))
+     decode(os,num)
